c9f201a3fa3ad8fec417bb74be9fcb9da7621820
Alicia Sykes <alicia.sykes@accenture.com>
2015-08-05 14:28:49 +0100

    Added mocha tests and coverage into gulp process

--- tasks/test.js
+++ tasks/test.js
@@ -1,9 +1,25 @@
 var gulp    = require('gulp');
 var mocha   = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+
 require('coffee-script/register')
 
+/* Run all tests and produce coverage report */
+gulp.task('test', function (cb) {
+    gulp.src(['source/**/*.js', 'main.js'])
+        .pipe(istanbul()) // Covering files
+        .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+        .on('finish', function () {
+            gulp.src(['test/*.{js,coffee}'])
+                .pipe(mocha())
+                .pipe(istanbul.writeReports()) // Creating the reports after tests ran
+                .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } })) // Enforce a coverage of at least 90%
+                .on('end', cb);
+        });
+});
+
 /* Run Mocha tests */
-gulp.task('test', function () {
+gulp.task('mocha', function () {
     return gulp.src('test/*.{js,coffee}')
         .pipe(mocha({compiler: 'nyan'}));
 });
