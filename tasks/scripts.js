6bf1eeac17f193e11a86a5c5edf5b0ef51609369
Alicia Sykes <alicia.sykes@accenture.com>
2015-08-15 18:30:21 +0100

    Improved scripts task, now using gulp-filter instead of event-stream

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -2,37 +2,40 @@ var gulp    = require('gulp');
 var gutil   = require('gulp-util');
 var gsize   = require('gulp-filesize');
 var jshint  = require('gulp-jshint');
-var concat  = require('gulp-concat');
 var uglify  = require('gulp-uglify');
 var coffee  = require('gulp-coffee');
 var cofLint = require('gulp-coffeelint');
 var changed = require('gulp-changed');
 var footer  = require('gulp-footer');
-var es      = require('event-stream');
+var filter  = require('gulp-filter');
 
 var CONFIG  = require('../tasks/config').CONFIG;
 
 /* JavaScript Tasks */
 gulp.task('scripts',  function(){
-    var jsSrcPath = CONFIG.SOURCE_ROOT + '/'+CONFIG.JS_SRC_DIR_NAME+'/**/*';
+
+    var coffeeFilter = filter('**/*.coffee', {restore: true});
+    var bundleFilter = filter(['*', '!**/*-main.{js,coffee}', '!**/*-main.{js,coffee}']);
+
+    var jsSrcPath = CONFIG.SOURCE_ROOT + '/'+CONFIG.JS_SRC_DIR_NAME+'/**/*.{coffee,js}';
     var jsResPath = CONFIG.DEST_ROOT + '/'+CONFIG.JS_DEST_DIR_NAME;
 
-    var jsFromCs = gulp.src(jsSrcPath+'.coffee')
+    return gulp.src(jsSrcPath)
+        .pipe(bundleFilter) // Ignore browserify files
+
+        /* CoffeeScript tasks */
+        .pipe(coffeeFilter)
         .pipe(cofLint())
         .pipe(cofLint.reporter())
-        .pipe(coffee());
+        .pipe(coffee())
+        .pipe(coffeeFilter.restore)
 
-    var jsFromPlain = gulp.src(jsSrcPath+'.js');
-
-    return es.merge(jsFromCs, jsFromPlain)
         .pipe(changed(jsResPath))
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
-        //.pipe(concat(CONFIG.JS_FILE_NAME,{newLine: ';'}))
         .pipe(uglify())
         .pipe(footer(CONFIG.FOOTER_TEXT))
         .pipe(gsize())
         .pipe(gulp.dest(jsResPath))
-        .on('error', gutil.log);
 
 });
