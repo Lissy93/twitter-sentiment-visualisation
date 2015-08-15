b5e75442c3c3d1029a9f6ec6dd10c2fe42e9fa08
Alicia Sykes <alicia.sykes@accenture.com>
2015-08-15 19:43:35 +0100

    Split browserify and script watch tasks for speed

--- tasks/browser-sync.js
+++ tasks/browser-sync.js
@@ -10,7 +10,11 @@ gulp.task('browser-sync', ['nodemon', 'browserify', 'scripts', 'styles', 'test']
         port: 4000,
         browser: ['google chrome']
     });
-    gulp.watch(CONFIG.SOURCE_ROOT+'/**/*.{js,coffee}', ['scripts', 'browserify', 'test']);
+    gulp.watch([
+        CONFIG.SOURCE_ROOT+'/**/*.{js,coffee}',
+            '!'+CONFIG.SOURCE_ROOT+'/**/*-{main,module}.{js,coffee}'],
+        ['scripts', 'test']);
+    gulp.watch(CONFIG.SOURCE_ROOT+'/**/*-{main,module}.{js,coffee}', ['browserify']);
     gulp.watch(CONFIG.SOURCE_ROOT+'/**/*.{css,less}',  ['styles']);
     gulp.watch(CONFIG.SOURCE_ROOT+"/**/*").on('change', bSync.reload);
     gulp.watch("views/**/*.jade").on('change', bSync.reload);
