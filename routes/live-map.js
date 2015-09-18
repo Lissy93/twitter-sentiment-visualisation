833e49c2988d2295308191cc4735f52fb8f49f76
Alicia Sykes <sykes.alicia@gmail.com>
2015-09-18 19:40:28 +0100

    Implemented live streaming Tweets with realtime awesomness

--- routes/live-map.js
+++ routes/live-map.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var React = require('react');
+var TweetsApp = React.createFactory(require('../components/TweetsApp'));
+var Tweet = require('../models/Tweet');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('page_map', { title: 'Map', pageNum: 2 });
+router.get('/', function(req, res) {
+    Tweet.getTweets(0,0, function(tweets) {
+
+        var reactHtml = React.renderToString(TweetsApp({tweets: tweets}));
+
+        res.render('page_livemap', {
+            title: 'Live Sentiment Map',
+            pageNum: 2,
+            markup: reactHtml,
+            state: JSON.stringify(tweets)
+        });
+    });
+
 });
 
 module.exports = router;
