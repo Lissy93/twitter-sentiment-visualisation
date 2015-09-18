2f86a17638e1b6e5e6701e7c8ba42ad44c337292
Alicia Sykes <sykes.alicia@gmail.com>
2015-09-18 19:33:55 +0100

    Connect to Mongodb and Socket.io TODO: refractor

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 /* Include necessary node modules */
 var express       = require('express');
 var path          = require('path');
@@ -6,6 +7,11 @@ var logger        = require('morgan');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 var mongoose      = require('mongoose');
+var http          = require('http');
+//var streamTweets  = require('stream-tweets'); //TODO fix package and re-add it
+var JSX           = require('node-jsx').install({extension: '.jsx'});
+
+var twitter       = require('ntwitter');
 
 /* Include the files defining the routes */
 var routes    = require('./routes/index');
@@ -13,13 +19,22 @@ var map       = require('./routes/map');
 var liveMap   = require('./routes/live-map');
 var timeline  = require('./routes/timeline');
 var about     = require('./routes/about');
+var streamHandler = require('./utils/stream-handler');
 
+/* Create Express server and configure socket.io*/
 var app = express();
+var port = process.env.PORT || 8080;
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(port, function(){
+    console.log('Express server listening on port ' + port);
+});
 
 /* view engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+/* Set up other Express bits and bobs */
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -31,12 +46,24 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 /* Connect to MongoDB */
 mongoose.connect('mongodb://localhost/twitter-sentiment-visualisations');
 
-/* Set routes */
+/* Specify which route files to use */
 app.use('/', routes);
 app.use('/map', map);
 app.use('/real-time-map', liveMap);
 app.use('/timeline', timeline);
 app.use('/about', about);
+
+
+/* Set a stream listener for tweets matching tracking keywords */
+var credentials = require('./config/keys').twitter;
+
+//var st = new streamTweets(credentials); //TODO add this back in once module is fixed
+var twit = new twitter(credentials);
+
+twit.stream('statuses/filter',{ track: 'hello'}, function(stream){
+    streamHandler(stream,io);
+});
+
 
 /* catch 404 and forward to error handler */
 app.use(function(req, res, next) {
@@ -66,6 +93,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 
 module.exports = app;
