37c555b982397a2d4c0bcec3e3cdc76d093598a2
Alicia Sykes <sykes.alicia@gmail.com>
2015-09-18 19:02:01 +0100

    Now includes reactify for jsx browserifying as well

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -2,6 +2,7 @@ var gulp    = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var coffeeify = require('coffeeify');
+var reactify = require('reactify');
 var buffer = require('vinyl-buffer');
 var sourcemaps = require('gulp-sourcemaps');
 var glob = require('glob');
@@ -14,10 +15,11 @@ var gsize   = require('gulp-filesize');
 var CONFIG  = require('../tasks/config').CONFIG;
 
 gulp.task('browserify', function (cb) {
-    glob('./'+CONFIG.SOURCE_ROOT+'/'+CONFIG.JS_SRC_DIR_NAME+'/**/**-main.{js,coffee}', function(err, files) {
+    glob('./'+CONFIG.SOURCE_ROOT+'/'+CONFIG.JS_SRC_DIR_NAME+'/**/**-main.{js,coffee,jsx}', function(err, files) {
         var tasks = files.map(function(entry) {
             return browserify({ entries:    [entry], debug: true })
                 .transform(coffeeify)
+                .transform(reactify)
                 .bundle()
                 .pipe(source(entry))
                 .pipe(rename(function(filepath) {
