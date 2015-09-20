ef476329a153ffffa142260a98d1b6c159f5fcbc
Alicia Sykes <sykes.alicia@gmail.com>
2015-09-20 07:36:47 +0100

    Code for generating new twitter keys file if not exist

---
+++ tasks/generate-config.js
@@ -0,0 +1,14 @@
+/* Include the necessary modules */
+var gulp    = require('gulp');
+var fs = require('fs');
+
+/* If configuration files don't exist, create them
+    (they'll still need populating with keys and credentials)  */
+gulp.task('generate-config',  function(){
+    fs.open('config/src/keys.coffee','r',function(err,fd){
+        if (err && err.code=='ENOENT') {
+            fs.createReadStream('config/src/sample-keys.coffee')
+                .pipe(fs.createWriteStream('config/src/keys.coffee'));
+        }
+    });
+});
