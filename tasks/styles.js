e57504d8c56da4010e12a253519781953e127932
Alicia Sykes <sykes.alicia@gmail.com>
2015-09-22 20:00:45 +0100

    Commented out temporarily not needed line

--- tasks/styles.js
+++ tasks/styles.js
@@ -4,8 +4,9 @@ var gutil   = require('gulp-util');
 var gsize   = require('gulp-filesize');
 var concat  = require('gulp-concat');
 var less    = require('gulp-less');
-var cssLint = require('gulp-csslint');
 var minCss  = require('gulp-minify-css');
+//var cssLint = require('gulp-csslint'); // Uncomment to lint CSS
+//var recess  = require('gulp-recess'); //TODO this isn't working, throws error
 var changed = require('gulp-changed');
 var footer  = require('gulp-footer');
 var es      = require('event-stream');
@@ -22,12 +23,16 @@ gulp.task('styles',  function(){
     var cssResPath = CONFIG.DEST_ROOT + '/'+CONFIG.CSS_DEST_DIR_NAME;
 
     var cssFromLess = gulp.src([cssSrcPath+'/*.less'])
+        //.pipe(recess())
+        //.pipe(recess.reporter())
         .pipe(less());
     var cssFromVanilla = gulp.src([cssSrcPath+'/*.css']);
 
     var excludedCss = gulp.src(cssSrcPath+'/*/*.css');
 
     var excludedLess = gulp.src(cssSrcPath+'/*/*.less')
+        //.pipe(recess())
+        //.pipe(recess.reporter())
         .pipe(less());
 
     var concatinatedCss = es.merge(cssFromLess, cssFromVanilla)
@@ -35,8 +40,8 @@ gulp.task('styles',  function(){
 
     return es.merge(concatinatedCss, excludedCss, excludedLess)
         .pipe(changed(cssResPath))
-        .pipe(cssLint())
-        .pipe(gulpIf(devMode, cssLint.reporter()))
+        //.pipe(cssLint()) // Uncomment to lint CSS
+        //.pipe(gulpIf(devMode, cssLint.reporter()))
         .pipe(minCss({compatibility: 'ie8'}))
         .pipe(gulpIf(devMode, gsize()))
         .pipe(footer(CONFIG.FOOTER_TEXT))
