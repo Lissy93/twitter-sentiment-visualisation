36dcd311de6746d894138ae6a19bbe3d1c1454c3
Alicia Sykes <sykes.alicia@gmail.com>
2016-04-09 09:10:37 +0100

    Removed unused var next

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(function(req, res, next) {
 
 /* development error handler (will print stacktrace) */
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
