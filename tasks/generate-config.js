95a51e5dfe7b06e53962634003eebc521c51f834
Alicia Sykes <sykes.alicia@gmail.com>
2017-03-18 21:50:45 +0000

    Updated paths in generate-config.js

--- tasks/generate-config.js
+++ tasks/generate-config.js
@@ -5,10 +5,10 @@ var fs = require('fs');
 /* If configuration files don't exist, create them
     (they'll still need populating with keys and credentials)  */
 gulp.task('generate-config',  function(){
-    fs.open('config/src/keys.coffee','r',function(err){
+    fs.open('server-side-source/config/keys.coffee','r',function(err){
         if (err && err.code==='ENOENT') {
-            fs.createReadStream('config/src/sample-keys.coffee')
-                .pipe(fs.createWriteStream('config/src/keys.coffee'));
+            fs.createReadStream('server-side-source/config/sample-keys.coffee')
+                .pipe(fs.createWriteStream('server-side-source/config/keys.coffee'));
         }
     });
 });
