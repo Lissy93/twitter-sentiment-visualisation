dbf625de932a73f39ff8bd5aaa728231b5e07e36
Alicia Sykes <sykes.alicia@gmail.com>
2018-03-25 23:08:50 +0100

    Oops, typo. This will now fix issue #21 🤞

--- tasks/file-mover.js
+++ tasks/file-mover.js
@@ -6,6 +6,6 @@ gulp.task('file-mover',  function(){
     return[
         gulp.src('client-side-source/manifest.json').pipe(gulp.dest('public')),
         gulp.src('client-side-source/data/regions.csv').pipe(gulp.dest('public/data')),
-        gulp.src('client-side-source/data/all_sentiment_resultsedwardsnowden.json').pipe(gulp.dest('public/data'))
+        gulp.src('client-side-source/data/all_sentiment_results_edwardsnowdon.json').pipe(gulp.dest('public/data'))
     ];
 });
