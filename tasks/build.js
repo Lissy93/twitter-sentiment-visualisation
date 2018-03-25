5d97171cf63ce6547f66870ba54c498808d06136
Alicia Sykes <sykes.alicia@gmail.com>
2018-03-25 23:00:56 +0100

    Added task to move static files during build

--- tasks/build.js
+++ tasks/build.js
@@ -4,6 +4,6 @@ var runSequence = require('run-sequence');
 /* Clean the work space */
 gulp.task('build',  function (cb) {
     runSequence('clean',
-        ['scripts', 'browserify', 'styles', 'images', 'manifest-move'],
+        ['scripts', 'browserify', 'styles', 'images', 'file-mover'],
         cb);
 });
--- tasks/file-mover.js
+++ tasks/file-mover.js
@@ -2,8 +2,10 @@
 var gulp    = require('gulp');
 
 /* Moves Manifest into public dir */
-gulp.task('manifest-move',  function(){
+gulp.task('file-mover',  function(){
     return[
         gulp.src('client-side-source/manifest.json').pipe(gulp.dest('public')),
+        gulp.src('client-side-source/data/regions.csv').pipe(gulp.dest('public/data')),
+        gulp.src('client-side-source/data/all_sentiment_resultsedwardsnowden.json').pipe(gulp.dest('public/data'))
     ];
 });
